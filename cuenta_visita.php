1314522aee32fc20ffec3310020e61ce39399468
Edwin Vasquez <edwinvasquez800@hotmail.com>
2017-04-16 10:22:05 -0500

    Fase Implantacion

---
+++ cuenta_visita.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contador</title>
+</head>
+
+<body>
+<?php
+    function contador()
+    {
+        $archivo = "contador.txt"; //el archivo que contiene en numero
+        $f = fopen($archivo, "r"); //abrimos el archivo en modo de lectura
+        if($f)
+        {
+            $contador = fread($f, filesize($archivo)); //leemos el archivo
+            $contador = $contador + 1; //sumamos +1 al contador
+            fclose($f);
+        }
+        $f = fopen($archivo, "w+");
+        if($f)
+        {
+            fwrite($f, $contador);
+            fclose($f);
+        }
+        return $contador;
+    }
+?>
+</body>
+</html>
